8b3e787a43ff28a0fa0ea4e060db5ea6d124049e
aakashsr <aakashsri.dev@gmail.com>
2020-05-21 09:11:45 +0530

    created a new module containing the functions to create and remove loader animation

--- js/popular.js
+++ js/popular.js
@@ -65,6 +65,30 @@ const viewController = (function () {
   };
 })();
 
+const base = (function () {
+  const renderLoader = function (parent) {
+    const loader = `
+    <div class='loader'>
+      <svg>
+        <use href='./img/icons.svg#icon-cw'></use>
+      </svg>
+    </div>
+    `;
+    parent.insertAdjacentHTML("afterbegin", loader);
+  };
+
+  const clearLoader = function () {
+    const loader = document.querySelector(".loader");
+    if (loader) {
+      loader.parentNode.removeChild(loader);
+    }
+  };
+  return {
+    renderLoader,
+    clearLoader,
+  };
+})();
+
 const controller = (function () {
   const state = {};
 
