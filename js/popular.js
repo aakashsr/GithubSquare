bde794f2cd80859eccf13c8fbe9260e8206bf03d
aakashsr <aakashsri.dev@gmail.com>
2020-05-21 17:23:26 +0530

    Saved all document.querySelectors into an object in base module.Provided comments at relevant places

--- js/popular.js
+++ js/popular.js
@@ -1,4 +1,3 @@
-// localStorage.clear();
 const searchController = (function () {
   function Search(query) {
     this.query = query;
@@ -43,7 +42,6 @@ const viewController = (function () {
      <div class="card-content card-small top-content user-card">
         <div class='user-image-container image-container-small'>
                 <a class='white' href=${obj.svn_url}><img src="${obj.owner.avatar_url}" alt="" class="user-image img-small"></a>
-               
             </div>
             <div class='user-info'>
                 <h2 class='name mb-medium'><a href=${obj.svn_url}>${obj.name}</a></h2>
@@ -52,7 +50,6 @@ const viewController = (function () {
                 <ul>
                     <li class="followers">
                         <div class='followers-count medium'>${obj.stargazers_count}</div>
-                       
                         <span class="followers-text small"><img class='color-star' src='./img/starfilled.png'></span>
                     </li>
                     <li class="stars">
@@ -66,23 +63,19 @@ const viewController = (function () {
                 </ul>
             </div>
             </div>`;
-      document.querySelector(".grid").insertAdjacentHTML("beforeend", html);
+      base.elements.grid.insertAdjacentHTML("beforeend", html);
     });
   }
 
   const showLanguages = function (data) {
-    document
-      .querySelector(".languages-container")
-      .insertAdjacentHTML("afterbegin", data);
+    base.elements.languagesContainer.insertAdjacentHTML("afterbegin", data);
   };
 
   const showNavigation = function () {
     let html = ` <button id='pagination-1' class='brk-btn pagination selected'>1</button>
             <button id='pagination-2' class='brk-btn pagination'>2</button>
             <button id='pagination-3' class='brk-btn pagination'>3</button>`;
-    document
-      .querySelector(".navigation")
-      .insertAdjacentHTML("afterbegin", html);
+    base.elements.navigation.insertAdjacentHTML("afterbegin", html);
   };
 
   return {
@@ -98,6 +91,10 @@ const viewController = (function () {
 const base = (function () {
   const elements = {
     loaderContainer: document.querySelector(".loader-container"),
+    loader: document.querySelector(".loader"),
+    languagesContainer: document.querySelector(".languages-container"),
+    grid: document.querySelector(".grid"),
+    navigation: document.querySelector(".navigation"),
   };
 
   const renderLoader = function (parent) {
@@ -125,6 +122,7 @@ const base = (function () {
 })();
 
 const controller = (function () {
+  // State of app
   const state = {};
 
   // save 'search' object into local storage
@@ -154,44 +152,41 @@ const controller = (function () {
 
       // -------- rendering lanuguages on app startup ------------- //
       // 1. clearing previous data
-      document.querySelector(".languages-container").innerHTML = "";
+      base.elements.languagesContainer.innerHTML = "";
       // 2. updating languages on refresh or startup
       viewController.showLanguages(state.search.languages);
 
-      // -------- rendering navigation on app startup -------------
-
+      // -------- rendering navigation on app startup ------------- //
       // i) clearing the buttons added previously
-      document.querySelector(".navigation").innerHTML = "";
+      base.elements.navigation.innerHTML = "";
       // ii) rendering the buttons( we haven't saved our buttons into state because we don't have any state attached to our buttons.Whenever we are refreshing , we just need to show all the three buttons because we always get the first page on refresh.We can't save 2nd or 3rd page result in localstorage)
-      viewSearchController.showNavigation(state.search.navigationContent);
+      viewController.showNavigation(state.search.navigationContent);
     }
   };
 
   const eventListeners = function () {
-    document
-      .querySelector(".languages-container")
-      .addEventListener("click", (e) => {
-        const languages = [
-          "All",
-          "JavaScript",
-          "Ruby",
-          "Java",
-          "C++",
-          "CSS",
-          "Python",
-          "Go",
-        ];
-        languages.forEach(function (cur) {
-          if (e.target.textContent === cur) {
-            handleResults(e);
-          }
-        });
+    base.elements.languagesContainer.addEventListener("click", (e) => {
+      const languages = [
+        "All",
+        "JavaScript",
+        "Ruby",
+        "Java",
+        "C++",
+        "CSS",
+        "Python",
+        "Go",
+      ];
+      languages.forEach(function (cur) {
+        if (e.target.textContent === cur) {
+          handleResults(e);
+        }
       });
+    });
 
     // setting event listener for navigation buttons
-    document
-      .querySelector(".navigation")
-      .addEventListener("click", (e) => handleNavigation(e));
+    base.elements.navigation.addEventListener("click", (e) =>
+      handleNavigation(e)
+    );
   };
 
   // Listener for languages buttons
@@ -209,14 +204,14 @@ const controller = (function () {
     state.search = new searchController.Search(query);
 
     // 5. clear previous result
-    document.querySelector(".grid").innerHTML = "";
+    base.elements.grid.innerHTML = "";
 
     // 6.render the loader
     base.renderLoader(base.elements.loaderContainer);
 
     // 7. clearning navigation so that as soon as loader starts , navigation get disappear
     // other wise navigation will move to top and will appear with loader
-    document.querySelector(".navigation").innerHTML = "";
+    base.elements.navigation.innerHTML = "";
 
     // 8. make the search by calling method saved on object's prototype
     await state.search.displayResults(endpoint);
@@ -228,7 +223,7 @@ const controller = (function () {
     viewController.showNavigation();
 
     // 11. Save all the languages
-    const languages = document.querySelector(".languages-container").innerHTML;
+    const languages = base.elements.languagesContainer.innerHTML;
     state.search.languages = languages;
 
     // 12. render the results once the data has come
@@ -248,7 +243,7 @@ const controller = (function () {
     e.target.classList.toggle("selected");
 
     // clearning prev results of page when user clicks on navigatoin btns"
-    document.querySelector(".grid").innerHTML = "";
+    base.elements.grid.innerHTML = "";
 
     let pageNumber = parseInt(e.target.textContent);
     viewController.showList(state.search.result, pageNumber);
@@ -257,8 +252,15 @@ const controller = (function () {
     setData();
   };
 
-  //   On first load , fetching the data and rendering on UI . Also rendering the navigation
+  //   On first app load , fetching the data and rendering on UI . Also rendering the navigation
   (async function onFirstLoad() {
+    // If it's very first load i,e local storage is null  -
+    // 1) create 'search' object
+    // 2) call 'displayResults' by passing 'all' into the endpoint
+    // 3) call 'showList' method to render items on UI.
+    // 4) Also show navigation and call event handler on it.
+
+    // NOTE: If you will only make api call and show items on UI using 'showList' then our navigation buttons will not be functional because they involved 'state.search.result' .So it'll say "can't find result of undefined"
     if (localStorage.getItem("data") === null) {
       state.search = new searchController.Search("all");
       const endpoint = `https://api.github.com/search/repositories?q=stars:>1+language:All&sort=stars&order=desc&type=Repositories`;
