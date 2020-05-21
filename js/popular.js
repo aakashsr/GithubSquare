0852e000974b30822165252e1140080735f268df
aakashsr <aakashsri.dev@gmail.com>
2020-05-21 07:17:28 +0530

    setup the event listener to our buttons in our main controller module

--- js/popular.js
+++ js/popular.js
@@ -2,4 +2,37 @@ const searchController = (function () {})();
 
 const viewController = (function () {})();
 
-const controller = (function () {})();
+const controller = (function () {
+  const eventListeners = function () {
+    document
+      .querySelector(".languages-container")
+      .addEventListener("click", (e) => {
+        const languages = [
+          "All",
+          "JavaScript",
+          "Ruby",
+          "Java",
+          "C++",
+          "CSS",
+          "Python",
+          "Go",
+        ];
+        languages.forEach(function (cur) {
+          if (e.target.textContent === cur) {
+            handleResults(e);
+          }
+        });
+      });
+  };
+
+  const handleResults = function (e) {
+    console.log("Running");
+  };
+
+  return {
+    init: function () {
+      eventListeners();
+    },
+  };
+})();
+controller.init();
