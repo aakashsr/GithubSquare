d99d3d2ef0a3bb2a49c7354502181bc000d2ab7c
aakashsr <aakashsri.dev@gmail.com>
2020-05-21 19:18:26 +0530

    setup event listeners to submit btn and getting value from viewController module

--- js/search.js
+++ js/search.js
@@ -1,3 +1,22 @@
 const searchController = (function () {})();
-const viewController = (function () {})();
-const controller = (function () {})();
+const viewController = (function () {
+  function getValue() {
+    return document.querySelector(".username").value;
+  }
+
+  return {
+    getValue,
+  };
+})();
+
+const controller = (function () {
+  document.getElementById("player-1").addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleSearch();
+  });
+
+  const handleSearch = function () {
+    // 1. get input value(username)
+    let username = viewController.getValue();
+  };
+})();
