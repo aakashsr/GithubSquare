d5b84b36b1fced0470be4875e047ec020ae75dee
aakashsr <aakashsri.dev@gmail.com>
2020-05-22 07:02:03 +0530

    created a new module called based that will render and clear loader and will select and store all elements in one object

--- js/search.js
+++ js/search.js
@@ -99,6 +99,31 @@ const viewController = (function () {
   };
 })();
 
+// base
+const base = (function () {
+  const renderLoader = function (parent) {
+    const loader = `
+    <div class='loader'>
+      <svg>
+        <use href='./img/icons.svg#icon-cw'></use>
+      </svg>
+    </div>
+    `;
+    parent.insertAdjacentHTML("afterbegin", loader);
+  };
+
+  const clearLoader = function () {
+    const loader = document.querySelector(".loader");
+    if (loader) {
+      loader.parentNode.removeChild(loader);
+    }
+  };
+  return {
+    renderLoader,
+    clearLoader,
+  };
+})();
+
 const controller = (function () {
   state = {};
 
