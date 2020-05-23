fc3d9fbcc8e2dff9871bb99f05cb99d27af84295
aakashsr <aakashsri.dev@gmail.com>
2020-05-23 14:15:10 +0530

    created a constructor function and bind a method on it's prototype that will make the request

--- js/trending.js
+++ js/trending.js
@@ -1,4 +1,17 @@
-const searchController = (function () {})();
+const searchController = (function () {
+  function Search(query) {
+    this.query = query;
+  }
+
+  Search.prototype.displayResults = async function (endpoint) {
+    const res = await axios(endpoint);
+    this.result = res.data.items;
+  };
+
+  return {
+    Search,
+  };
+})();
 const viewController = (function () {})();
 
 // main controller module
