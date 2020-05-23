9c235094b823a061ce1e60f9bce0749ab027a3b7
aakashsr <aakashsri.dev@gmail.com>
2020-05-23 15:06:58 +0530

    added event listener to both - main categores and selectmenu

--- js/trending.js
+++ js/trending.js
@@ -4,8 +4,19 @@ const searchController = (function () {
   }
 
   Search.prototype.displayResults = async function (endpoint) {
-    const res = await axios(endpoint);
-    this.result = res.data.items;
+    try {
+      const reposData = await axios(`https://ghapi.huchen.dev/repositories`);
+      const developersData = await axios(`https://ghapi.huchen.dev/developers`);
+      console.log(reposData.data);
+      console.log(developersData.data);
+      this.repos = reposData.data;
+      this.developers = developersData.data;
+      console.log(reposData.data);
+      console.log(developersData.data);
+      return reposData.data;
+    } catch (e) {
+      return `We've an error here: ${e}`;
+    }
   };
 
   return {
@@ -16,7 +27,13 @@ const viewController = (function () {})();
 
 // main controller module
 const controller = (function () {
+  // state
+  const state = {};
   // Event listeners
+
+  document
+    .querySelector(".categories")
+    .addEventListener("click", (e) => handleMain(e));
 
   // To toggle select menu
   document.querySelector(".selected").addEventListener("click", () => {
@@ -31,13 +48,24 @@ const controller = (function () {
       ).innerHTML;
       document.querySelector(".options-container").classList.remove("active");
 
-      handleResult(item);
+      handleCategories(item);
     });
   });
 
-  function handleResult(item) {
+  async function handleMain(e) {
+    console.log("handlecommon");
+  }
+
+  async function handleCategories(item) {
     // 1. get the query
     let query = item.querySelector("label").innerHTML;
-    console.log(query);
+
+    // 2. create a new object and save in state
+    state.trending = new searchController.Search(query);
+
+    // 3. make the request(search)
+    await state.trending.displayResults();
+
+    // console.log(state);
   }
 })();
